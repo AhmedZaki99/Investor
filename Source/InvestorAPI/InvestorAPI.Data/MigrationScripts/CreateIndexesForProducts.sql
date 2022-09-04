f1319ef2c5b3db946e053b584f1693af7ac0185f
Ahmed Zaki <Ahmed.Z13@outlook.com>
2022-09-04 02:00:23 +0200

    Added migrations to create Product indexes in the database.

---
+++ Source/InvestorAPI/InvestorAPI.Data/MigrationScripts/CreateIndexesForProducts.sql
@@ -0,0 +1,25 @@
+﻿BEGIN TRANSACTION;
+GO
+
+CREATE UNIQUE INDEX [IX_ScaleUnits_Name] ON [ScaleUnits] ([Name]);
+GO
+
+CREATE UNIQUE INDEX [IX_Products_Code] ON [Products] ([Code]) WHERE [Code] IS NOT NULL;
+GO
+
+CREATE INDEX [IX_Products_IsService] ON [Products] ([IsService]);
+GO
+
+CREATE UNIQUE INDEX [IX_Products_Name] ON [Products] ([Name]);
+GO
+
+CREATE UNIQUE INDEX [IX_Categories_Name] ON [Categories] ([Name]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220903235252_CreateIndexesForProducts', N'6.0.8');
+GO
+
+COMMIT;
+GO
+
